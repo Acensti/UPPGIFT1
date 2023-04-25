c8443b569e832763b1ba12588ab9c9c1d708a7a0
Acensti <37810260+Acensti@users.noreply.github.com>
2023-04-25 23:26:30 +0200

    Revert "Moves things to the new folder"
    This reverts commit 23762ee2982847c7d6e0318d5699afcc2f803b9a.

---
+++ Backend/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+using Backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<ContactForm> ContactForms { get; set; }
+        // Add other DbSets here as needed
+    }
+}
---
+++ Backend/Models/Product.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public bool IsFeatured { get; set; }
+        public bool IsNew { get; set; }
+        public bool IsPopular { get; set; }
+    }
+}
